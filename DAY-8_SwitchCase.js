fe89cb463b8c284f4ac95d7ffdc5007f5b769c89
Dhruvraj16 <137039673+Dhruvraj16@users.noreply.github.com>
2023-06-27 15:46:30 +0530

    Add files via upload

---
+++ DAY-8_SwitchCase.js
@@ -0,0 +1,27 @@
+var month=13;
+switch(month)
+{
+    case 1:
+        document.write("January");
+        break;
+    case 2:
+        document.write("February");
+        break;
+    default: 
+    document.write("Wrong Input!");
+        break;       
+}
+
+var month="July";
+switch(month)
+{
+    case "June";
+    document.write("sixth Month||")|
+    break;
+    case "July";
+    document.write("seventh Month||")|
+    break;
+}
+
+var a=true;
+switch(a)
