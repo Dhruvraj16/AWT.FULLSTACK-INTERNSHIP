ae76eaa5dee738cc794ee8c38c7367814cf2f760
Dhruvraj16 <137039673+Dhruvraj16@users.noreply.github.com>
2023-06-27 15:45:34 +0530

    Add files via upload

---
+++ DAY 8/DAY-8_Else_If.js
@@ -0,0 +1,10 @@
+let a=10;
+if (a < 5) {
+    document.write("a is less than 5.");
+}
+else if(a=10) {
+    document.write("a is equal to 10.");
+}
+else {
+    document.write("a is greater than 5.");
+}
