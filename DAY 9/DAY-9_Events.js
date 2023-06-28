dd1a097a53782279db81abd4ad0713ea0c790bf6
Dhruvraj16 <137039673+Dhruvraj16@users.noreply.github.com>
2023-06-28 15:26:25 +0530

    Add files via upload

---
+++ DAY 9/DAY-9_Events.js
@@ -0,0 +1,20 @@
+function demoOnClick(){
+    alert("On Click");
+}
+function demoOnDb1Click(){
+    alert("On Double Click");
+}
+function demoOnFocus(){
+    document.getElementById("txtFocus").style.backgroundColor="Red";
+}
+function demoOnBlur(){
+    document.getElementById("txtBlur").style.backgroundColor="Yellow";
+}
+function demoOnKeypress() {
+    alert("Keypress Done");
+}
+function demoOnKeyup() {
+    alert("Keyup Done");
+}
+    
+
